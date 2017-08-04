8929b67bb8b6c4de0b50527c69489315f10bb973
aman5252 <30698910+aman5252@users.noreply.github.com>
2017-08-04 22:32:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/artcle-one', function (req, res){
+    res.send('Article one will be served here');
+});
+app.get('/artcle-two', function (req, res){
+    res.send('Article two will be served here');
+});
+app.get('/artcle-three', function (req, res){
+    res.send('Article three will be served here');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
