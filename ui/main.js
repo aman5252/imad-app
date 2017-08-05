f3814a7e8f8c797d60e9bdf545a4dd28fbb2f6a5
aman5252 <30698910+aman5252@users.noreply.github.com>
2017-08-05 21:11:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ element.innerHTML='New value';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'pix';
+    marginLeft= marginLeft+1;
+    img.style.marginLeft= marginLeft+'px';
     
 }
 img.onclick=function()
 {
-    var interval=setInterval(moveRight,50);
+    var interval= setInterval(moveRight,50);
 };
