a4b8c3abc00215d586a5ec3abcf42a83e3b67a9b
aman5252 <30698910+aman5252@users.noreply.github.com>
2017-08-07 12:18:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
+//var element=document.getElementById('main-text');
+//element.innerHTML='New value';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
