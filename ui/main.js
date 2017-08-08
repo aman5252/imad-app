f1003042a81337ed112bbc391b551ffdce216475
aman5252 <30698910+aman5252@users.noreply.github.com>
2017-08-08 19:53:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,3 +12,23 @@ img.onclick=function()
 {
     var interval= setInterval(moveRight,50);
 };
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function()
+{
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function()
+  {
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {
+            var counter=request.responseText;
+            var span=documen.getElementById('count');
+            span.innerHtml=counter.toString();
+        }
+    }
+  };
+  request.open('Get','http://apsmiglani.imad.hasura-app.io/counter',true);
+  request.send(null);
+};
