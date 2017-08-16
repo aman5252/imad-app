2f1b7eed6ff5d683d268fd69b0abf9d12f57b4f7
aman5252 <30698910+aman5252@users.noreply.github.com>
2017-08-16 21:08:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('/submit-name', function (req, res) {
   
 });
 
-app.get('/articles/:articleName', function (req, res){
+app.get('/article/:articleName', function (req, res){
    
    pool.query("SELECT * FROM article WHERE title= $1", [req.params.articleName],function(err,result){
        if(err) {
