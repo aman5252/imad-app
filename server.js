e596067573ca9fce0c444f51a4c2c8b16bb7a9e6
aman5252 <30698910+aman5252@users.noreply.github.com>
2017-08-21 20:27:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user: 'apsmiglani',
    database: 'apsmiglani',
@@ -100,6 +101,16 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+function hash(input,salt)
+{
+    // How do we create a hash?
+    var hashed=crypto.pbkdf25ync(input,salt,10000,512,'shah512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function (req, res){
+   var hashedString =hash(req.params.input,'this-is-some-random-string');
+   res.send(hashedString);
 });
 var pool=new Pool(config);
 app.get('/test-db', function (req, res) {
